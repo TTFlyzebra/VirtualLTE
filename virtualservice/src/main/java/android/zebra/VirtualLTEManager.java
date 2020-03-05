b57addecc7e67daccb56bab4252031075554d776
flyzebra <flycnzebra@gmail.com>
2020-03-05 12:29:21 +0800

    edit ui

--- virtualservice/src/main/java/android/zebra/VirtualLTEManager.java
+++ virtualservice/src/main/java/android/zebra/VirtualLTEManager.java
@@ -2,7 +2,6 @@ package android.zebra;
 
 import android.content.Context;
 import android.os.RemoteException;
-import android.zebra.IVirtualLTEService;
 
 public class VirtualLTEManager
 {
@@ -11,6 +10,13 @@ public class VirtualLTEManager
     public void setVirtualLTEInfo(VlteLanInfo vlteLanInfo)
     {
         try {
+            SystemPropTools.set("persist.sys.vlte.wifissid",vlteLanInfo.wifissid);
+            SystemPropTools.set("persist.sys.vlte.wifipsk",vlteLanInfo.wifipsk);
+            SystemPropTools.set("persist.sys.vlte.ipAddress",vlteLanInfo.ipAddress);
+            SystemPropTools.set("persist.sys.vlte.ipMask",vlteLanInfo.ipMask);
+            SystemPropTools.set("persist.sys.vlte.gateway",vlteLanInfo.gateway);
+            SystemPropTools.set("persist.sys.vlte.dns1",vlteLanInfo.dns1);
+            SystemPropTools.set("persist.sys.vlte.dns2",vlteLanInfo.dns2);
             mService.setVirtualLTEInfo(vlteLanInfo.toJsonString());
         } catch (RemoteException e) {
             FlyLog.e("openVirtualLTE error!");
